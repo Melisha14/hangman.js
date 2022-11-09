5d8bb13e37b7bcece0a49670f29852c13bf1502f
Melisha14 <105646979+Melisha14@users.noreply.github.com>
2022-11-09 04:46:34 +0100

    Commit

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-var v1;
+/*var v1;
 var v2=6;
 
 v1="frj";
 
 console.log(v1,v2); var v1;
-var v2=6;
 
-v1="frj";
+*/
 
-console.log(v1,v2);
+var alphabet=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'VW', 'w', 'X', 'Y', 'Z'];
+
+
+
+/*
+liste de mots : LIVERPOOL, BATMAN, HUMOUR, LIVRE, ANIMAL, FILM, JOURNAL, AQUARIUM, APOCALYPSE, BOUCHE
+*/
